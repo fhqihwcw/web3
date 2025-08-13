2c7c059f0cc74a33eeda372c898489b640f9205a
zhaoxj <80507309@qq.com>
2025-08-13 16:29:42 +0800

    任务1

--- task1/main.go
+++ task1/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 )
 
 func main() {
@@ -11,8 +12,28 @@ func main() {
 	// ip := isPalindrome(1234)
 	// fmt.Println(ip)
 
-	vs := validString("(([))][]{}")
-	fmt.Println(vs)
+	// vs := validString("(([))][]{}")
+	// fmt.Println(vs)
+
+	// str := []string{"flower", "flow", "flight"}
+
+	// flag := longestCommonPrefix(str)
+	// fmt.Println(flag)
+
+	digits := []int{1, 2, 9}
+	plus := plusOne(digits)
+	fmt.Println(plus)
+
+	length, nums := removeDuplicates([]int{0, 0, 1, 1, 1, 2, 2, 3, 3, 4})
+	fmt.Println(length, nums)
+
+	nums = []int{2, 7, 11, 15}
+	flag := twoSum(nums, 18)
+	fmt.Println(flag)
+
+	intervals := [][]int{{1, 3}, {2, 6}, {8, 10}, {15, 18}, {17, 20}}
+	merged := merge(intervals)
+	fmt.Println(merged)
 }
 
 /*
@@ -105,6 +126,22 @@ func validString(s string) bool {
 链接：https://leetcode-cn.com/problems/longest-common-prefix/
 */
 
+func longestCommonPrefix(strs []string) string {
+	if len(strs) == 0 {
+		return ""
+	}
+	prefix := strs[0]
+	for i := 1; i < len(strs); i++ {
+		for len(prefix) > 0 && len(strs[i]) < len(prefix) || strs[i][:len(prefix)] != prefix {
+			prefix = prefix[:len(prefix)-1]
+			if prefix == "" {
+				return ""
+			}
+		}
+	}
+	return prefix
+}
+
 /**
 基本值类型
 加一
@@ -113,6 +150,22 @@ func validString(s string) bool {
 题目：给定一个由整数组成的非空数组所表示的非负整数，在该数的基础上加一
 链接：https://leetcode-cn.com/problems/plus-one/
 **/
+
+func plusOne(digits []int) []int {
+
+	temp := ""
+	for _, digit := range digits {
+		temp += fmt.Sprintf("%d", digit)
+	}
+	num, _ := strconv.Atoi(temp)
+	num++
+	str := strconv.Itoa(num)
+	for i := 0; i < len(str); i++ {
+		digits[i], _ = strconv.Atoi(string(str[i]))
+	}
+	return digits
+
+}
 
 /**
 引用类型：切片
@@ -123,17 +176,53 @@ func validString(s string) bool {
 链接：https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array/
 **/
 
-/**
+func removeDuplicates(nums []int) (int, []int) {
+	temp := []int{}
+	for i := 0; i < len(nums); i++ {
+		if i == 0 || nums[i] != nums[i-1] {
+			temp = append(temp, nums[i])
+		}
+	}
+	return len(temp), temp
+}
+
+/*
+*
 56. 合并区间：以数组 intervals 表示若干个区间的集合，其中单个区间为 intervals[i] = [starti, endi] 。
 请你合并所有重叠的区间，并返回一个不重叠的区间数组，该数组需恰好覆盖输入中的所有区间。
 可以先对区间数组按照区间的起始位置进行排序，然后使用一个切片来存储合并后的区间，遍历排序后的区间数组，
 将当前区间与切片中最后一个区间进行比较，如果有重叠，则合并区间；如果没有重叠，则将当前区间添加到切片中。
 */
+func merge(intervals [][]int) [][]int {
+	temp := [][]int{}
+	if len(intervals) == 0 {
+		return temp
+	}
+	for i := 0; i < len(intervals); i++ {
+		if len(temp) == 0 || temp[len(temp)-1][1] < intervals[i][0] {
+			temp = append(temp, intervals[i])
+		} else {
+			temp[len(temp)-1][1] = max(temp[len(temp)-1][1], intervals[i][1])
+		}
+	}
+	return temp
+}
 
-/**
+/*
+*
 基础
 两数之和
 考察：数组遍历、map使用
 题目：给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那两个整数
 链接：https://leetcode-cn.com/problems/two-sum/
 */
+func twoSum(nums []int, target int) []int {
+	m := make(map[int]int)
+	for i, num := range nums {
+		if j, ok := m[target-num]; ok {
+			return []int{j, i}
+		}
+		m[num] = i
+	}
+	return nil
+}
